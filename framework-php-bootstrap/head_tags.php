7419d6888411786dec544021294b4ea54e83e8ce
THorne93 <taylorhorne1993@gmail.com>
2024-11-04 10:22:20 +0100

    head tags

---
+++ framework-php-bootstrap/head_tags.php
@@ -0,0 +1,26 @@
+<title><?php print $PAGE_TITLE;?></title>
+
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta name="description" content="Groundsound" />
+<meta name="author" content="Brogrammers" />
+
+
+<!--<link rel="icon" type="image/x-icon" href="assets/favicon.ico" /> TODO
+
+
+
+<!-- Google fonts-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Krona+One&family=Quantico&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Krona+One&family=Maven+Pro:wght@400..900&family=Quantico&display=swap" rel="stylesheet">
+<!-- Core theme CSS (includes Bootstrap)-->
+
+<link href="css/styles.css" rel="stylesheet" />
+
+//Include Boostrap JS for interactive components
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+

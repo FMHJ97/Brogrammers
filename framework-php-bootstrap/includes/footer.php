e6cc23cd9bd2783955147b5aae25a03c995dffa7
FMHJ97 <francisco-h-j@hotmail.com>
2024-11-06 13:06:50 +0100

    feat(FMHJ.footer): footer updated

--- framework-php-bootstrap/includes/footer.php
+++ framework-php-bootstrap/includes/footer.php
@@ -2,24 +2,32 @@
     <div class="container">
         <div class="row d-flex flex-column">
             <!-- Social Media Icons -->
-            <div class="col">
+            <div id="divSocialMedia" class="col">
                 <h4>Síguenos</h4>
-                <div id="divIcons" class="row row-cols-1 row-cols-md-2">
-                    <div class="col text-center text-md-end">
-                        <!-- Instagram -->
-                        <a class="mx-1" href="https://www.instagram.com/"><i class="bi bi-instagram text-light"></i></a>
-                        <!-- TikTok -->
-                        <a class="mx-1" href="https://www.tiktok.com/"><i class="bi bi-tiktok text-light"></i></a>
-                        <!-- X -->
-                        <a class="mx-1" href="https://www.x.com/"><i class="bi bi-twitter-x text-light"></i></a>
+                <div class="row row-cols-3 row-cols-md-6">
+                    <!-- Instagram -->
+                    <div class="col">
+                        <a href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
                     </div>
-                    <div class="col text-center text-md-start">
-                        <!-- Youtube -->
-                        <a class="mx-1" href="https://www.youtube.com/"><i class="bi bi-youtube text-light"></i></a>
-                        <!-- Facebook -->
-                        <a class="mx-1" href="https://www.facebook.com/"><i class="bi bi-facebook text-light"></i></a>
-                        <!-- Email -->
-                        <a class="mx-1" href="https://www.gmail.com/"><i class="bi bi-envelope-fill text-light"></i></a>                        
+                    <!-- TikTok -->
+                    <div class="col">
+                        <a href="https://www.tiktok.com/"><i class="bi bi-tiktok"></i></a>
+                    </div>
+                    <!-- X -->
+                    <div class="col">
+                        <a href="https://www.x.com/"><i class="bi bi-twitter-x"></i></a>
+                    </div>
+                    <!-- YouTube -->
+                    <div class="col">
+                        <a href="https://www.youtube.com/"><i class="bi bi-youtube"></i></a>
+                    </div>
+                    <!-- Facebook -->
+                    <div class="col">
+                        <a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>
+                    </div>
+                    <!-- Email -->
+                    <div class="col">
+                        <a href="https://www.gmail.com/"><i class="bi bi-envelope-fill"></i></a>
                     </div>
                 </div>
             </div>

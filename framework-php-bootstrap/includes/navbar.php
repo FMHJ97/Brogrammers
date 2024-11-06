7db21dec14ddb009ff4d6cb50419a333d3608702
JosePonferrada <joseponfe10@gmail.com>
2024-11-06 21:18:01 +0100

    fix(bro): added the burguer menu but is not working well. To be fixed

--- framework-php-bootstrap/includes/navbar.php
+++ framework-php-bootstrap/includes/navbar.php
@@ -4,6 +4,40 @@
             <img src="../assets/img/Logo.svg" alt="Festival Logo"> 
         </a>
     </div>
+
+    <!-- The hamburguer menu -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="./lineup.php">LineUp</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./tickets.php">Tickets</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./merch.php">Merch</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./gallery.php">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./info.php">Info</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link cart-icon" href="./cart.php"><i class="bi bi-cart2"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./login.php"><i class="bi bi-person-circle"></i></a>
+            </li>
+        </ul>
+    </div>
+<!-- 
     <div class="container-fluid justify-content-end ">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
@@ -29,4 +63,6 @@
             </li>
         </ul>
     </div>
+
+-->
 </nav>

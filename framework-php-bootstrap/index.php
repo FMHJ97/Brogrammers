a5adc8ad1f06de1e887fce196c6feaa51aa33a9b
THorne93 <taylorhorne1993@gmail.com>
2024-11-17 17:03:34 +0100

    Timer finished, carousel started, buttons started

--- framework-php-bootstrap/index.php
+++ framework-php-bootstrap/index.php
@@ -5,90 +5,6 @@
 <head>
   <?php include("includes/head_tags.php"); ?>
 </head>
-
-<body>
-  <?php include("includes/navbar.php"); ?>
-
-  <main>
-    <div class="container-fluid">
-      <div class="row p-0 position-relative">
-        <img class="img-fluid p-0 opacity-50" src="/assets/img/index/mainPhoto.jpg" alt="Hay un Imagen aqui" />
-
-        <div class="position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center">
-          <div id="timer" class="text-white text-center d-flex justify-content-center">
-            <div class="time-block d-flex align-items-center">
-              <div class="time-item">
-                <div class="time-number" id="days">000</div>
-                <div class="time-label">DAYS</div>
-              </div>
-              <div class="separator">:</div>
-              <div class="time-item">
-                <div class="time-number" id="hours">00</div>
-                <div class="time-label">HRS</div>
-              </div>
-              <div class="separator">:</div>
-              <div class="time-item">
-                <div class="time-number" id="minutes">00</div>
-                <div class="time-label">MINS</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-
-
-      <div class="row">
-        <button>buy your tickets now</button>
-      </div>
-      <!--Carrusel-->
-      <div class="row">
-        <div class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img
-                class="d-block w-100"
-                src="https://placehold.co/600x400?text=1"
-                alt="First slide" />
-            </div>
-            <div class="carousel-item">
-              <img
-                class="d-block w-100"
-                src="https://placehold.co/600x400?text=2"
-                alt="Second slide" />
-            </div>
-            <div class="carousel-item">
-              <img
-                class="d-block w-100"
-                src="https://placehold.co/600x400?text=3"
-                alt="Third slide" />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <button>Line up</button>
-      </div>
-
-      <div class="row">
-        <div>AQUI VA EL LOGO</div>
-        <div>AQUI VA LA FECHA</div>
-        <div>AQUI VA LA DIRECCIÓN</div>
-      </div>
-
-      <div class="row">
-        <button>buy your tickets now</button>
-      </div>
-
-    </div>
-    <?php include("includes/patrocinadores.php");  ?>
-  </main>
-
-  <?php include("includes/footer.php"); ?>
-</body>
 <script>
   // Target date for the countdown
   const targetDate = new Date("August 15, 2025 00:00:00").getTime();
@@ -117,5 +33,101 @@
   // Start the countdown and update it every second
   setInterval(updateCountdown, 1000);
 </script>
+
+<body>
+  <?php include("includes/navbar.php"); ?>
+
+  <main>
+    <div class="container-fluid">
+      <div class="row p-0 position-relative">
+        <img class="img-fluid p-0 opacity-50" src="/assets/img/index/mainPhoto.jpg" alt="Hay un Imagen aqui" />
+
+        <div class="timer-overlay start-0">
+          <div id="timer" class="timer-container">
+            <div class="time-block d-flex align-items-center">
+              <div class="time-item">
+                <div class="time-number" id="days">000</div>
+                <div class="time-label">DAYS</div>
+              </div>
+              <div class="time-item">
+                <div class="time-number">:</div>
+                <div class="time-label invisible">A</div> <!-- Use "invisible" to hide while keeping the space -->
+              </div>
+              <div class="time-item">
+                <div class="time-number" id="hours">00</div>
+                <div class="time-label pr-1">HRS</div>
+              </div>
+              <div class="time-item">
+                <div class="time-number">:</div>
+                <div class="time-label invisible">A</div> <!-- Use "invisible" to hide while keeping the space -->
+              </div>
+              <div class="time-item">
+                <div class="time-number" id="minutes">00</div>
+                <div class="time-label">MINS</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+      <div class="row my-3 py-3">
+        <button type="button" class="btn-index ">BUY YOUR TICKETS NOW</button>
+      </div>
+      <!--Carrusel-->
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="5000">
+            <img class="d-block w-100" src="assets/img/index/2.gif" alt="First slide">
+          </div>
+          <div class="carousel-item" data-bs-interval="5000">
+            <img class="d-block w-100" src="assets/img/index/1.gif" alt="Second slide">
+          </div>
+          <div class="carousel-item " data-bs-interval="5000">
+            <img class="d-block w-100" src="assets/img/index/3.gif" alt="Third slide">
+          </div>
+          <div class="carousel-item " data-bs-interval="5000">
+            <img class="d-block w-100" src="assets/img/index/4.gif" alt="Fourth slide">
+          </div>
+          <div class="carousel-item " data-bs-interval="5000">
+            <img class="d-block w-100" src="assets/img/index/5.gif" alt="Fifth slide">
+          </div>
+          <div class="carousel-item " data-bs-interval="5000">
+            <img class="d-block w-100" src="assets/img/index/6.gif" alt="Sixth slide">
+          </div>
+          <div class="carousel-item " data-bs-interval="5000">
+            <img class="d-block w-100" src="assets/img/index/7.gif" alt="Seventh slide">
+          </div>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <button>Line up</button>
+      </div>
+
+      <div class="row">
+        <div>AQUI VA EL LOGO</div>
+        <div>AQUI VA LA FECHA</div>
+        <div>AQUI VA LA DIRECCIÓN</div>
+      </div>
+
+
+      <div class="row my-3 py-3">
+        <button type="button" class="btn-index ">BUY YOUR TICKETS NOW</button>
+      </div>
+
+    </div>
+    <?php include("includes/patrocinadores.php");  ?>
+  </main>
+
+  <?php include("includes/footer.php"); ?>
+</body>
+
 
 </html>

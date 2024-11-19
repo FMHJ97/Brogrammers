3c986e10ca40fb85dfff60d585d8aa472597b3a7
Fran Ruiz Martin <145322849+FRuiMar@users.noreply.github.com>
2024-11-19 13:06:38 +0100

    feat(info.php)

--- framework-php-bootstrap/info.php
+++ framework-php-bootstrap/info.php
@@ -11,133 +11,40 @@
     <!-- Navigation Bar -->
     <?php include("includes/navbar.php"); ?>
 
-    <main class="mb-5">
-        <!-- PRIVACY POLICY -->
-        <section id="privacy" class="container page-section">
-            <!-- Título fuera del recuadro --><!-- PRIVACY POLICY -->
-            <div class="container page-section-heading  mb-2">
-                <h1 class="text-center">INFO</h1>
-            </div>
-
-            <!-- Recuadro blanco con contenido --><!-- PRIVACY POLICY -->
-            <div class="container bg-white text-black shadow-sm rounded-3 ">
-                <h4 class="text-center mb-4 py-3">TRATAMIENTO DE DATOS PERSONALES Y POLÍTICA DE PRIVACIDAD</h4>
-                <div class="mb-4">
-                    <h5><b>1. ¿QUIÉN ES EL RESPONSABLE DEL TRATAMIENTO DE TUS DATOS?</b></h5>
-                    <p>
-                        <b>Identidad:</b> GroundSound S.L.U. B06660666<br>
-                        <b>Dirección postal:</b> C/ El Peso 666, Lucena 14900<br>
-                        <b>Correo electrónico:</b> info@groundsound.luc
-                    </p>
-                </div>
-                <div class="mb-4">
-                    <h5><b>2. ¿CON QUÉ FINALIDAD TRATAMOS TUS DATOS PERSONALES?</b></h5>
-                    <p>
-                        Cualquier tipo de petición que sea realizada por el usuario a través de cualquiera de las formas de contacto que se ponen a disposición del usuario en el presente sitio web. Además, su dirección de correo electrónico puede utilizarse para remitir el boletín de noticias de la página web, así como comunicaciones comerciales sobre los servicios que puedan ser de su interés, si así se estima oportuno y siempre que el titular haya confirmado de forma expresa su consentimiento para tal fin.
-                    </p>
-                </div>
-                <div class="mb-4">
-                    <h5><b>3. ¿POR CUÁNTO TIEMPO CONSERVAMOS TUS DATOS?</b></h5>
-                    <p>
-                        Los datos personales proporcionados se conservarán mientras no se nos solicite su supresión, hasta un máximo de 6 años.
-                    </p>
-                </div>
-                <div class="mb-4">
-                    <h5><b>4. ¿CUÁL ES LA LEGITIMACIÓN PARA EL TRATAMIENTO DE TUS DATOS?</b></h5>
-                    <p>
-                        La base legal para el tratamiento de tus datos es el consentimiento expreso que nos proporcionas al aceptar los diferentes formularios de la web o bien, y en caso de contratar con nosotros algún servicio, la ejecución de la relación contractual que sea pertinente.
-                    </p>
-                </div>
-                <div class="mb-4">
-                    <h5><b>5. ¿A QUÉ DESTINATARIOS SE COMUNICARÁN TUS DATOS?</b></h5>
-                    <p>
-                        Los datos podrán ser tratados por diversos encargados de tratamiento con el fin de poder utilizar los servicios de la web. Dichos encargados de tratamiento se encuentran legitimados para tratar sus datos en la ejecución del contrato del encargo con cláusula de confidencialidad incluida. Los encargados de tratamiento en ningún caso podrán ceder tus datos.
-                    </p>
-                    <p>
-                        Relación de encargados de tratamiento usados para la gestión y uso de la web:<br>
-                        <b>Google, INC:</b> A través de su servicio de Google Analytics como herramienta de analítica web. Más información.<br>
-                        <b>Gigas Hosting S.A.:</b> Gestor de correo y hosting web. Más información.
-                    </p>
-                </div>
-                <div class="mb-4 pb-2">
-                    <h5><b>6. ¿CUÁLES SON TUS DERECHOS CUANDO NOS FACILITAS TUS DATOS?</b></h5>
-                    <p>
-                        Cualquier persona tiene derecho a obtener confirmación sobre si estamos tratando datos personales que le conciernan o no. Las personas interesadas tienen derecho a acceder a sus datos personales, así como a solicitar la rectificación de los datos inexactos o, en su caso, solicitar su supresión cuando, entre otros motivos, los datos ya no sean necesarios para los fines que fueron recogidos.
-                    </p>
-                    <p>
-                        En determinadas circunstancias, los interesados podrán solicitar la limitación del tratamiento de sus datos, en cuyo caso únicamente los conservaremos para el ejercicio o la defensa de reclamaciones. En determinadas circunstancias y por motivos relacionados con su situación particular, los interesados podrán oponerse al tratamiento de sus datos. Y si es así, dejaremos de tratar los datos, salvo por motivos legítimos imperiosos o el ejercicio o la defensa de posibles reclamaciones.
-                    </p>
-                    <p>
-                        <b>Procedimiento:</b> Podrás ejercitar estos derechos por carta (acompañada de fotocopia de D.N.I.) a la dirección:<br>
-                        <b>GroundSound S.L.U.</b><br>
-                        Departamento de protección de datos<br>
-                        Dirección: C/ El Peso 666, Lucena 14900 (España)
-                    </p>
-                </div>
+    <main>
+        <!-- INFO Heading -->
+        <section class="container page-section">
+            <div class="row page-section-heading">
+                <h1>INFO</h1>
             </div>
         </section>
-
-
-        <!-- TERMS AND CONDITIONS. -->
-        <section id="terms" class="container page-section">
-            <!-- Título fuera del recuadro --><!-- TERMS AND CONDITIONS. -->
-            <div class="container page-section-heading  mb-2">
-                <h1 class="text-center">TERMS AND CONDITIONS</h1>
-            </div>
-
-            <!-- Recuadro blanco con contenido --><!-- TERMS AND CONDITIONS. -->
-            <div class="container bg-white text-black shadow-sm  pt-3 pb-2 rounded-3">
-                <h4 class="text-center mb-4 py-3">TERMS Y CONDITIONS</h4>
-                <div class="mb-4">
-                    <p>
-                        In addition to Front Gate Tickets other terms and conditions, I understand and acknowledge that all ticket purchases for any Danny Wimmer Presents, LLC event are final and non-refundable for any reason.
-                        I understand that if I do not attend the Event or the Event does not happen or is canceled in whole or in part for any reason, I may not receive a refund of any portion of my ticket purchase. This includes if all or part of the event is canceled due to dangerous weather, pandemic or other reasons outside of the festivals control. Festival performers are subject to change or cancellation at any time without notice. No refund will be owed if a festival performer is changed or cancelled.
-                        At the sole discretion of Danny Wimmer Presents, LLC (“DWP”), I may receive a partial or full credit (“Credit”) that I can use towards the purchase of a ticket to another festival produced by DWP. After I receive the Credit, if any, it must be used within 365 days. Use of the Credit is subject to the availability of tickets for the other DWP and I am not guaranteed a ticket to the festival of my choice.
-                        DWP is committed to the health and safety of all patrons and staff. Enhanced safety measures will be in place based on the latest guidance from city, county and state government health officials and will follow industry standards to ensure everyone can relax and enjoy the festival. Patrons and staff will be required to follow all safety precautions for admittance. All safety measures will be published prior to the event, posted at all entrances and enforced throughout the event.
-                        An inherent risk of exposure to COVID-19 exists in any place where people gather. COVID-19 is an extremely contagious disease that can lead to severe illness and death. You assume all risks, hazards, and dangers arising from or relating in any way to the risk of contracting COVID-19 or any other communicable disease or illness, or a bacteria, virus or other pathogen capable of causing a communicable disease or illness, whether occurring before, during, or after the event, however caused or contracted, and voluntarily waive all claims and potential claims against DWP, Event Partners, and their affiliated companies relating to such risks. COVID-19 health and safety protocols (including the requirement of a negative test and/or vaccination) may be implemented at any time. The execution of said protocols are assumed by the purchaser and do not open the event to chargebacks or refunds.By attending the Festival, a public event, you acknowledge and agree that your appearance and your actions inside and outside the venue where the Festival occurs are public in nature, and that you have no expectation of privacy with regard to your actions or conduct at the Festival. As a term of your ticket purchase, you grant permission the Festival to utilize your name, image, likeness, acts, poses, plays, appearance, movements, and statements in any live or recorded audio, video, or photographic display or other transmission, exhibition, publication or reproduction made of, or at, the Festival (regardless of whether before, during or after play or performance) for any purpose, in any manner, in any medium or context now known or hereafter developed, without further authorization from, or compensation to, you or anyone acting on your behalf.
-                    </p>
+        <!--Seleccion Info -->
+        <div class="container">
+            <div class="row btn-category-group px-0 justify-content-center">
+                <div class="col-lg-3 col-6 mb-2">
+                    <button type="button" id="history" class="btn btn-category-item selected w-100">Fest History</button>
                 </div>
-
+                <div class="col-lg-3 col-6 mb-2">
+                    <button type="button" id="tickets" class="btn btn-category-item w-100">Tickets Info</button>
+                </div>
+                
+                <div class="col-lg-3 col-6 mb-2">
+                    <button type="button" id="camping" class="btn btn-category-item w-100">Camping</button>
+                </div>
+                <div class="col-lg-3 col-6 mb-2">
+                    <button type="button" id="accessibility" class="btn btn-category-item w-100">Accessibility</button>
+                </div>
             </div>
+        </div>
+
 
         </section>
-
-
-
-
-
-
-        <!-- CONTACT US -->
-        <section id="contact" class="container page-section">
-            <!-- Título fuera del recuadro --> <!-- CONTACT US -->
-            <div class="container page-section-heading  mb-2">
-                <h1 class="text-center">CONTACT US</h1>
+        <!-- CONTENEDOR INFO 1 -->
+        <section class="container page-section">
+            <!-- First Row -->
+            <div class="row row-cols-1 row-cols-md-3 merch-products">
+                
             </div>
-
-            <!-- Recuadro blanco con contenido --> <!-- CONTACT US -->
-            <div class="container bg-white text-black shadow-sm rounded-3">
-                <div class="mb-5">
-                    <h4 class="pt-3">CUSTOMER SERVICE & ALL GENERAL ENQUIRIES</h4>
-                    <p>Please contact <a href="mailto:info@groundsound.luc">info@groundsound.luc</a></p>
-                </div>
-
-                <div class="mb-4">
-                    <h5>LOST PROPERTY</h5>
-                    <p>Please contact <a href="mailto:lostandfound@groundsound.luc">lostandfound@groundsound.luc</a></p>
-                </div>
-
-                <div class="mb-4">
-                    <h5>MEDIA ENQUIRES</h5>
-                    <p>Please contact <a href="mailto:gsupstream@groundsound.luc">gsupstream@groundsound.luc</a></p>
-                </div>
-
-                <div class="mb-4 pb-2">
-                    <h5>ACCESSIBILITY ENQUIRIES</h5>
-                    <p class="mb-3">Please contact <a href="mailto:gsupstream@groundsound.luc">gsupstream@groundsound.luc</a></p>
-                </div>
-            </div>
-
-
         </section>
 
     </main>

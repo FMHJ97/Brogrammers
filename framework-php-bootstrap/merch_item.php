2765382d853fbcbee336026426bb6abfef9c0f48
FMHJ97 <francisco-h-j@hotmail.com>
2024-11-21 23:34:59 +0100

    feat(FMHJ): item_merch updated

--- framework-php-bootstrap/merch_item.php
+++ framework-php-bootstrap/merch_item.php
@@ -57,9 +57,9 @@
                     </div>
                     <!-- Merch Item Add to Cart Button -->
                     <div class="row">
-                        <div class="col">
+                        <div class="col py-2">
                             <form action="#" method="POST">
-                                <button type="submit" class="btn btn-cart">Añadir al carrito</button>
+                                <button type="submit" class="btn btn-cart">Añadir al Carrito</button>
                             </form>
                         </div>
                     </div>
@@ -78,9 +78,43 @@
                 </div>
             </div>
         </section>
-        <!-- Suggestions Section -->
-        <section class="container page-section">
-
+        <!-- Suggested Products Section -->
+        <section class="container page-section px-3 px-md-5">
+            <!-- Suggestions Heading -->
+             <div class="row">
+                <div class="col pb-4">
+                    <h2>También te puede interesar</h2>
+                </div>
+             </div>
+            <!-- Suggested Products -->
+            <div class="row merch-products">
+                <!-- Merch Item 1 -->
+                <a href="./merch_item.php" class="col-12 col-md-4 card card-merch-item all-items clothes">
+                    <img class="card-img-top" src="./assets/img/merch/camiseta_1.png"
+                        alt="Camiseta GroundSound Blanca (Black Logo)">
+                    <div class="card-body">
+                        <h3 class="card-title">Camiseta GroundSound Blanca (Black Logo)</h3>
+                        <span>€25,00 EUR</span>
+                    </div>
+                </a>
+                <!-- Merch Item 2 -->
+                <a href="./merch_item.php" class="col-12 col-md-4 card card-merch-item all-items clothes">
+                    <img class="card-img-top" src="./assets/img/merch/camiseta_2.png"
+                        alt="Camiseta GroundSound Negra (Neon Logo)">
+                    <div class="card-body">
+                        <h3 class="card-title">Camiseta GroundSound Negra (Neon Logo)</h3>
+                        <span>€30,00 EUR</span>
+                    </div>
+                </a>
+                <!-- Merch Item 3 -->
+                <a href="./merch_item.php" class="col-12 col-md-4 card card-merch-item all-items accesories">
+                    <img class="card-img-top" src="./assets/img/merch/bolsa.png" alt="Mochila Saco GroundSound Negra">
+                    <div class="card-body">
+                        <h3 class="card-title">Mochila Saco GroundSound<br>Negra</h3>
+                        <span>€15,00 EUR</span>
+                    </div>
+                </a>
+            </div>
         </section>
     </main>
 

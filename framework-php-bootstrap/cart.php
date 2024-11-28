367a1e285134058044442d33f989618374929ed7
JosePonferrada <joseponfe10@gmail.com>
2024-11-28 20:14:28 +0100

    fix(JMPA): fixed cart table to make it responsive

--- framework-php-bootstrap/cart.php
+++ framework-php-bootstrap/cart.php
@@ -8,14 +8,14 @@
 </head>
 
 <body>
-    <!-- Navigation Bar -->
+    <!-- Barra de navegación -->
     <?php include("includes/navbar.php"); ?>
 
     <main>
         <section class="page-section">
 
-            <div class="container ">
-                <!-- Row with titles (Carrito, dirección, pago) -->
+            <div class="container">
+                <!-- Fila con títulos (Carrito, Dirección, Pago) -->
                 <div class="row d-flex text-center page-section-heading">
                     <div class="col">
                         <h3 class="step-title active">Carrito</h3>
@@ -30,117 +30,75 @@
                     </div>
                 </div>
 
-                <!-- Row with articles and summary -->
-
+                <!-- Fila con artículos y resumen -->
                 <div class="row g-3">
-                    <div class="col-md-8 ">
+                    <div class="col-md-8">
+                        <div class="table-responsive-md"> <!-- Hace que la tabla sea responsive -->
+                            <!-- Títulos de la tabla -->
+                            <table class="table table-cart table-borderless table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <h5>Artículo</h5>
+                                        </th>
+                                        <th class="text-center">
+                                            <h5>Precio</h5>
+                                        </th>
+                                        <th class="text-center">
+                                            <h5>Cantidad</h5>
+                                        </th>
+                                        <th>
+                                            <h5>Subtotal</h5>
+                                        </th>
+                                    </tr>
+                                </thead>
 
-                        <!-- Titles -->
-                        <table class="table table-cart table-borderless table-striped">
-                            <thead>
+                                <!-- Cada fila representa un artículo -->
                                 <tr>
-                                    <th><h5>Artículo</h5></th>
-                                    <th class="text-center"><h5>Precio</h5></th>
-                                    <th class="text-center"><h5>Cantidad</h5></th>
-                                    <th><h5>Subtotal</h5></th>
+                                    <td>
+                                        <img src="../assets/img/merch/camiseta_1.png" alt="Camiseta" class="img-article-cart">
+                                        <div>
+                                            <p class="mb-0">Camiseta GroundSound</p>
+                                            <p class="mb-0">Color - Blanco</p>
+                                            <p class="mb-0">Talla - L</p>
+                                        </div>
+                                    </td>
+                                    <td class="text-center align-middle">25€</td>
+                                    <td class="align-middle">
+                                        <div class="item-quantity-cart">
+                                            <button type="button" id="restar" class="btn btn-quantity-cart">-</button>
+                                            <span id="quantity-cart">1</span>
+                                            <button type="button" id="sumar" class="btn btn-quantity-cart">+</button>
+                                        </div>
+                                    </td>
+                                    <td class="text-center align-middle">50€</td>
                                 </tr>
-                            </thead>
 
-                            <!-- Each tr is a row with an article -->
-                            <tr>
-
-                                <td>
-                                    <img src="../assets/img/merch/camiseta_1.png" alt="Camiseta" class="img-article-cart">
-
-                                    <div>
-                                        <p class="mb-0">Camiseta GroundSound</p>
-                                        <p class="mb-0">Color - Blanco</p>
-                                        <p class="mb-0">Talla - L</p>
-                                    </div>
-
-                                </td>
-
-                                <td class="text-center align-middle">25€</td>
-
-                                <td class="align-middle">
-                                    <div class="item-quantity-cart">
-                                        <button type="button" id="restar" class="btn btn-quantity-cart">-</button>
-                                        <span id="quantity-cart">1</span>
-                                        <button type="button" id="sumar" class="btn btn-quantity-cart">+</button>
-                                    </div>
-                                </td>
-
-                                <td class="text-center align-middle">50€</td>
-
-                            </tr>
-
-                            <!-- Each tr is a row with an article -->
-                            <tr>
-
-                                <td>
-                                    <img src="../assets/img/merch/camiseta_1.png" alt="Camiseta"
-                                        class="img-article-cart">
-                                    <!-- To place the text in the same line as the image -->
-
-                                    <div>
-                                        <p class="mb-0">Camiseta GroundSound</p>
-                                        <p class="mb-0">Color - Blanco</p>
-                                        <p class="mb-0">Talla - L</p>
-                                    </div>
-
-                                </td>
-
-                                <td class="text-center align-middle">25€</td>
-
-                                <td class="align-middle">
-                                    <div class="item-quantity-cart">
-                                        <button type="button" id="restar" class="btn btn-quantity-cart">-</button>
-                                        <span id="quantity-cart">1</span>
-                                        <button type="button" id="sumar" class="btn btn-quantity-cart">+</button>
-                                    </div>
-                                </td>
-
-                                <td class="text-center align-middle">50€</td>
-
-                            </tr>
-
-                            <!-- Each tr is a row with an article -->
-                            <tr>
-
-                                <td>
-                                    <img src="../assets/img/merch/camiseta_1.png" alt="Camiseta"
-                                        class="img-article-cart">
-                                    <!-- To place the text in the same line as the image -->
-
-                                    <div>
-                                        <p class="mb-0">Camiseta GroundSound</p>
-                                        <p class="mb-0">Color - Blanco</p>
-                                        <p class="mb-0">Talla - L</p>
-                                    </div>
-
-                                </td>
-
-                                <td class="text-center align-middle">25€</td>
-
-                                <td class="align-middle">
-                                    <div class="item-quantity-cart">
-                                        <button type="button" id="restar" class="btn btn-quantity-cart">-</button>
-                                        <span id="quantity-cart">1</span>
-                                        <button type="button" id="sumar" class="btn btn-quantity-cart">+</button>
-                                    </div>
-                                </td>
-
-                                <td class="text-center align-middle">50€</td>
-
-                            </tr>
-
-                        </table>
-
-
-
+                                <!-- Repetimos para más artículos -->
+                                <tr>
+                                    <td>
+                                        <img src="../assets/img/merch/camiseta_1.png" alt="Camiseta" class="img-article-cart">
+                                        <div>
+                                            <p class="mb-0">Camiseta GroundSound</p>
+                                            <p class="mb-0">Color - Blanco</p>
+                                            <p class="mb-0">Talla - L</p>
+                                        </div>
+                                    </td>
+                                    <td class="text-center align-middle">25€</td>
+                                    <td class="align-middle">
+                                        <div class="item-quantity-cart">
+                                            <button type="button" id="restar" class="btn btn-quantity-cart">-</button>
+                                            <span id="quantity-cart">1</span>
+                                            <button type="button" id="sumar" class="btn btn-quantity-cart">+</button>
+                                        </div>
+                                    </td>
+                                    <td class="text-center align-middle">50€</td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
 
-                    <!-- Summary -->
+                    <!-- Resumen del pedido -->
                     <div class="col-md-4 container-summary p-3">
                         <div class="row mb-3">
                             <div class="col text-center">
@@ -150,10 +108,9 @@
 
                         <div class="row">
                             <div class="col">
-
                                 <p>Detalles del pedido</p>
-
                                 <div class="row">
+                                    <!-- Cada col-8 representa la columna de la izquierda y cada col-4 la de la derecha -->
                                     <div class="col-8 text-end">
                                         <p>Precio sin IVA</p>
                                     </div>
@@ -161,7 +118,6 @@
                                         <p>50€</p>
                                     </div>
                                 </div>
-
                                 <div class="row">
                                     <div class="col-8 text-end">
                                         <p>IVA</p>
@@ -170,7 +126,6 @@
                                         <p>10€</p>
                                     </div>
                                 </div>
-
                                 <div class="row">
                                     <div class="col-8 text-end">
                                         <p>Cupón de descuento</p>
@@ -179,7 +134,6 @@
                                         <p>0€</p>
                                     </div>
                                 </div>
-
                                 <div class="row">
                                     <div class="col-8 text-end">
                                         <p>Gastos de envío</p>
@@ -188,47 +142,36 @@
                                         <p>5€</p>
                                     </div>
                                 </div>
-
                             </div>
-
                         </div>
-
 
                         <p class="mb-0 discount-text">¿Tienes un código de descuento?</p>
                         <div class="row mb-3">
-
                             <div class="col-md-7 mb-1">
                                 <input type="text" class="form-control-discount" placeholder="Introduzca código">
                             </div>
-
                             <div class="col-md-5">
                                 <button type="button" class="button-apply-discount">Aplicar</button>
                             </div>
-
                         </div>
 
                         <div class="row">
-
                             <div class="col-8">
                                 <h5>Total</h5>
                             </div>
-
                             <div class="col-4 text-end">
                                 <h5>65€</h5>
                             </div>
                         </div>
 
                         <a href="address.php" class="button-ticket">Continuar</a>
-
                     </div>
-
                 </div>
             </div>
-
         </section>
     </main>
 
-    <!-- Footer -->
+    <!-- Pie de página -->
     <?php include("includes/footer.php"); ?>
 
 </body>

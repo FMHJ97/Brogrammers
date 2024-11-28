0c0ce4c7f4014cfca019af65a6bd0c78007e546c
THorne93 <taylorhorne1993@gmail.com>
2024-11-28 21:06:28 +0100

    feat(TH): added comments to relevant parts, moved styles to more adequate places.

--- framework-php-bootstrap/gallery.php
+++ framework-php-bootstrap/gallery.php
@@ -11,29 +11,31 @@
 <body>
     <?php include("includes/navbar.php"); ?>
     <main>
-    <section class="container page-section">
-        <div class="row page-section-heading">
-            <h1>GALLERY</h1>
+
+        <section class="container page-section">
+            <div class="row page-section-heading">
+                <h1>GALLERY</h1>
+            </div>
+        </section>
+        <div class="container page-section">
+            <!-- Todos los imagenes estan cargado a traves de javascript   -->
+            <div id="mediaContainer" class="grid row mt-4">
+            </div>
+            <div class="row my-3 px-4 py-3">
+                <!-- Esto en el paso de funcionalidad es posible que se cambie -->
+                <button href="" id="addImages" class="btn-gallery mx-auto my-3">↓ see more ↓</button>
+            </div>
+
         </div>
-    </section>
-    <div class="container page-section">
-        <div id="mediaContainer" class="grid row mt-4">
-        </div>
-        <div class="row my-3 px-4 py-3">
+        <div class="container page-section">
             <!-- Esto en el paso de funcionalidad es posible que se cambie -->
-            <button href="" id="addImages" class="btn-gallery mx-auto my-3">↓ see more ↓</button>
+            <div class="row my-3 py-3">
+                <button id="submitImageTODO" class="btn-index ">Send us your pics</button>
+            </div>
+
         </div>
-
-    </div>
-    <div class="container page-section">
-
-        <div class="row my-3 py-3">
-            <button id="submitImageTODO" class="btn-index ">Send us your pics</button>
-        </div>
-
-    </div>
     </main>
-   
+
 
 
 

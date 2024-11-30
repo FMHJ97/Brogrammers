85294c71f1ca906243168315bfbceefecabe53b6
Fran Ruiz Martin <franruiz81@gmail.com>
2024-11-30 12:30:46 +0100

    Revisando comentarios por si acaso

--- framework-php-bootstrap/includes/patrocinadores.php
+++ framework-php-bootstrap/includes/patrocinadores.php
@@ -1,13 +1,11 @@
 <!-- PATROCINADORES -->
 <section class="container ">
-  <!--Creo un segundo CONTAINER para darle margen frente al tamaño por defecto que 
-  tiene el Container principal cuando se pone en modo móvil-->
-  <div class="container patro-seccion"> <!--CORRECCIÓN AÑADIDA-->
+  <section class="patro-seccion"> 
     <!-- TITULO-->
     <div class="row my-3 d-flex" id="patro-titulo">
-      <h1>PATROCINADORES</h1>
+      <h2>PATROCINADORES</h2>
     </div>
-    <!-- IMAGENES PATROCINADORES-->
+    <!-- ROW con IMAGENES PATROCINADORES-->
     <div class="row  mx-auto my-4" id="patro-fila">
       <div class="col-12 col-md-3 imgpatro">
         <img src="../assets/img/patrocinadores/estrella-galicia-10.png">
@@ -22,6 +20,5 @@
         <img src="../assets/img/patrocinadores/wolf-pit0.png" />
       </div>
     </div>
-  </div>
-
+  </section>
 </section>

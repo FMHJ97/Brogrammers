c5fa0e91f57ca750935e820d6b8d8180a1aee21f
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-01-08 09:33:33 +0100

    feat(FMHJ): legal.php updated

--- framework-php-bootstrap/legal.php
+++ framework-php-bootstrap/legal.php
@@ -24,6 +24,23 @@
             <div class="row terms">
                 <div class="col">
                     <h2>1. INFORMACIÓN GENERAL</h2>
+                    <div>
+                        En cumplimiento con el deber de información dispuesto en la Ley 34/2002 de Servicios de la
+                        Sociedad de
+                        la Información y el Comercio Electrónico (LSSI-CE) de 11 de julio, se facilitan a continuación
+                        los siguientes
+                        datos de información general de este sitio web:
+                        La titularidad de este sitio web, www.groundsound.com, (en adelante, Sitio Web) la ostenta:
+                        GROUNDSOUND S.L., provista de NIF: B-06660666 e inscrita en: Registro Mercantil de Córdoba con
+                        los
+                        siguientes datos registrales: Tomo 3, Página 7, Hoja 279, cuyo representante es: , y cuyos datos
+                        de
+                        contacto son:
+                        Dirección: C/ El Peso 666, Lucena 14900 (Córdoba)
+                        Teléfono de contacto: 957525818
+                        Email de contacto: groundsoundfestival@gmail.com
+                    </div>
+                    <h2></h2>
                 </div>
             </div>
         </section>

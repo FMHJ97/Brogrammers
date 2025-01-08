852db8999589e6de766c8a9da5b571472e3216c0
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-01-08 09:28:09 +0100

    feat(legal.php): file updated

---
+++ framework-php-bootstrap/legal.php
@@ -0,0 +1,38 @@
+<?php include("includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head_tags.php"); ?>
+    <script src="./js/scripts.js"></script>
+</head>
+
+<body>
+    <!-- Navigation Bar -->
+    <?php include("includes/navbar.php"); ?>
+
+    <main>
+
+        <section class="container page-section">
+            <div class="row page-section-heading">
+                <h1>Términos Legales</h1>
+            </div>
+        </section>
+
+        <!-- TERMS AND CONDITIONS. -->
+        <section class="container page-section">
+            <div class="row terms">
+                <div class="col">
+                    <h2>1. INFORMACIÓN GENERAL</h2>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <!-- Footer -->
+    <?php include("includes/footer.php"); ?>
+
+</body>
+
+</html>

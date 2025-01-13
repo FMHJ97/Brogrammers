5e04a917509b84dd7b3bee7e5b58d2fbe4057817
Fran Ruiz Martin <franruiz81@gmail.com>
2025-01-13 01:36:47 +0100

    dddd

--- framework-php-bootstrap/juegoFranRuiz.php
+++ framework-php-bootstrap/juegoFranRuiz.php
@@ -7,7 +7,6 @@
     <script src="https://cdn.jsdelivr.net/npm/howler"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <style>
-        /* Al ser un juego le meto los estilos a parte???*/
         body {
             margin: 0;
             background-color: #000;

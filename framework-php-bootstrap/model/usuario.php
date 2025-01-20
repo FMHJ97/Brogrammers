1fd6f801ecd458f512b1d10ce7a808763abb1f1d
THorne93 <taylorhorne1993@gmail.com>
2025-01-20 10:14:56 +0100

    feat(TH) usuario model/controller (insert + select)

---
+++ framework-php-bootstrap/model/usuario.php
@@ -0,0 +1,49 @@
+<?php
+
+class Usuario {
+
+    private $id;
+    private $username;
+    private $clave;
+    private $nombre;
+    private $apellido1;
+    private $apellido2;
+    private $correo;
+    private $fecha_nac;
+    private $pais;
+    private $codigo_postal;
+    private $telefono;
+    private $img_perfil;
+    private $rol;
+
+    public function __construct($id,$u,$cl,$n,$a1,$a2,$co,$p,$cp,$t,$i,$r) {
+        $this->id = $id;
+        $this->username = $u;
+        $this->clave = $cl;
+        $this->nombre = $n;
+        $this->apellido1 = $a1;
+        $this->apellido2 = $a2;
+        $this->correo = $co;
+        $this->pais = $p;
+        $this->codigo_postal = $cp;
+        $this->telefono = $t;
+        $this->img_perfil = $i;
+        $this->rol = $r;
+    }
+
+    public function __get(string $name): mixed {
+        return $this->$name;
+    }
+
+    public function __set(string $name, mixed $value): void {
+        $this->$name = $value;
+    }
+
+    public function __toString()
+    {
+        return "Soy usuario";
+    }
+    
+}
+
+?>

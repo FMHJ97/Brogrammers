6f7730e3ca334f3183f099fa7ab6d9eaf2c8ba8e
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-01-28 10:43:40 +0100

    feat(FMHJ): BD updated

--- scriptConInserts.sql
+++ scriptConInserts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-01-2025 a las 09:38:05
+-- Tiempo de generación: 28-01-2025 a las 10:40:22
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -75,15 +75,16 @@ CREATE TABLE `producto` (
   `nombre` text DEFAULT NULL,
   `descripcion` text DEFAULT NULL,
   `detalles` text DEFAULT NULL,
-  `precio` decimal(10,2) DEFAULT NULL
+  `precio` decimal(10,2) DEFAULT NULL,
+  `categoria` enum('ropa','accesorio','musica') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `detalles`, `precio`) VALUES
-(1, 'Producto de prueba', 'Descripción de prueba', 'Detalles de prueba', 10.00);
+INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `detalles`, `precio`, `categoria`) VALUES
+(1, 'Producto de prueba', 'Descripción de prueba', 'Detalles de prueba', 10.00, 'ropa');
 
 -- --------------------------------------------------------
 
@@ -112,8 +113,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `apellido1`, `apellido2`, `correo_electronico`, `clave`, `fecha_nac`, `pais`, `codigo_postal`, `telefono`, `img_perfil`, `newsletter`, `rol`) VALUES
-(1, 'Pepe', 'García', 'Pérez', 'pepe@gmail.com', '$2y$10$9v9mR7p9hRoQIYFy0wfZQ.isHsRVHjAEFqKPcpYLktN6MSugddp6i', '1990-01-01', 'España', '28001', '666666666', NULL, 0, 'admin'),
-(2, 'Jason', 'Jason', '', 'jason@gmail.com', '$2y$10$ngsEQdGfWS.EyZo0TIiNtu9rKNd6vv4w5YDG2rMbGT9Iq8YWHxTIu', '1996-12-12', 'España', '14500', '666666666', NULL, 0, 'usuario');
+(1, 'Admin', 'Admin', 'Admin', 'admin@gmail.com', '$2y$10$j.FMwRlh82OxeI/YJtrCluuEXvBYLCLfVmQ.zSO.IbOc54GVW6Tsu', '1990-10-10', 'España', '14500', '639410944', NULL, 0, 'admin');
 
 -- --------------------------------------------------------
 

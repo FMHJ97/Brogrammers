6a4b11f224bab2f31af21258e7a326e936681813
Jose Ponferrada <joseponfe10@gmail.com>
2025-02-04 11:49:27 +0100

    fix(JMPA): yet to finish msgs alerts

--- framework-php-bootstrap/js/register.js
+++ framework-php-bootstrap/js/register.js
@@ -6,3 +6,23 @@ document.addEventListener("DOMContentLoaded", function() {
         btnCrearCuenta.disabled = !this.checked;
     });
 });
+
+const alertPlaceholder = document.getElementById('mensajeAlert')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('btnCrearCuenta')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('No se ha podido registrar. Revise los datos introducidos!', 'danger')
+  })
+}

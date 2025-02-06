d9175dbcae94979c2e6df94485824f7ce3e44c4e
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-02-06 14:28:22 +0100

    feat(FMHJ): DB updated

--- groundsound.sql
+++ groundsound.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de datos: `groundsound`
 --
 
+create database groundsound;
+use groundsound;
+
 -- --------------------------------------------------------
 
 --

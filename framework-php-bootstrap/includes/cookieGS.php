6111cbdaba799f50a4f64680ff5886a5961ed03d
Fran Ruiz Martin <franruiz81@gmail.com>
2025-02-06 01:15:19 +0100

    CookiesYNews

--- framework-php-bootstrap/includes/cookieGS.php
+++ framework-php-bootstrap/includes/cookieGS.php
@@ -1,7 +1,7 @@
-    <!-- Cartel de cookies -->
+<!-- Cartel de cookies -->
 
-    <!--  <div id="bloqueoBanner" class="bloqueoalert">-->
-    <!-- 
+<!--  <div id="bloqueoBanner" class="bloqueoalert">-->
+<!-- 
     
     <div id="cookieBanner" class="text-center cookiealert alert" role="alert">
         <img class="GSImageLogo" src="../assets/img/Logo.svg" alt="GroundSound">
@@ -18,41 +18,87 @@
     
     
     -->
-    <!--  </div> -->
+<!--  </div> -->
 
 
-    <div id="cookieBanner" class="text-center alert cookiealert" role="alert">
-        <img class="GSImageLogo" src="../assets/img/Logo.svg" alt="GroundSound">
-        <p id="cookieText">
-            Utilizamos cookies propias y de terceros para mejorar la experiencia del usuario en nuestra
-            web. ¿Aceptas el uso de cookies?
-        </p>
-        <button id="acceptcookie" type="button" class="btn acceptcookies" aria-label="Close">
+<div id="cookieBanner" class="text-center alert cookieBanner" role="alert">
+    <img class="GSImageLogo" src="../assets/img/Logo.svg" alt="GroundSound">
+    <p id="cookieText">
+        Utilizamos cookies propias y de terceros para mejorar la experiencia del usuario en nuestra
+        web. ¿Aceptas el uso de cookies?
+    </p>
+    <div class="botones-cookies">
+        <button id="acceptcookie" type="button" class="btn" aria-label="Close">
             Aceptar
         </button>
         <button id="rejectcookie" type="button" class="btn" aria-label="Close">
             Rechazar
         </button>
     </div>
+    <a class="enlaceCookie" href="./cookies.php" target="_blank" rel="noopener noreferrer">Política de Cookies</a>
+</div>
+<div class="blockWeb" id="blockWeb"></div>
 
 
 
 
-    <div id="newsBanner" class="text-center alert newsbanner" role="alert">
-        <p id="newsText">
-            ¡Novedades! Aquí tienes las últimas actualizaciones.
-        </p>
-        <button id="closenews" type="button" class="btn" aria-label="Close">
-            Cerrar
-        </button>
+<div id="newsBanner" class="text-center alert newsbanner" role="alert">
+    <div class="header">
+        <img class="GSImageLogoNews" src="../assets/img/Logo.svg" alt="GroundSound">
+        <h1 id="newsTextHeader">
+            ¡Novedades!
+        </h1>
+        <h2> Aquí tienes las últimas actualizaciones.</h2>
     </div>
+    <div class="boxNew" id="boxNew">
+        <div class="news " id="news">
+            <div>
+                <p id="newsText">
+                    No te pierdas las nuevas gorras de Ground Sound. ¡Hazte con la tuya!
+                </p>
+                <p id="newsText">
+                    Pincha en la imagen o accede a nuestra <a class="enlaceNews" href="./merch.php" target="_blank"
+                        rel="noopener noreferrer">zona de merchandising</a>.
+                </p>
+            </div>
+            <a href="./merch_item.php?id=8" class="col-12 col-md-4 card card-merch-item-News all-items">
+                <img class="card-img-top-News" src="./assets/img/merch/gorra.png" alt="Gorra GroundSound">
+                <div class="card-body-News">
+                    <h3 class="card-title card-title-News">Gorra
+                        GroundSound (Golden Dream)</h3>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="boxNew" id="boxNew">
+        <div class="news " id="news">
+            <div>
+                <p id="newsText">
+                    Nueva incorporación a GroundSound Festival. 2Pac vuelve del más allá para un concierto único.
+                <p id="newsText">
+                    Revisa todos nuestros artistas en nuestro <a class="enlaceNews" href="./lineup.php" target="_blank"
+                        rel="noopener noreferrer">cartel del festival</a>.
+                </p>
+            </div>
+            <div class="col-12 col-md-4 card card-merch-item-News all-items">
+                <img class="card-img-top-News" src="./assets/img/lineUp/2pac.jpg" alt="2Pac">
+                <div>
+                    <h3 class="card-title card-title-News">2Pac</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button id="closenews" type="button" class="btn" aria-label="Close">
+        Leido
+    </button>
+</div>
 
 
 
 
 
 
-    <!-- <script src="../js/cookiesGSs.js"></script> -->
+<!-- <script src="../js/cookiesGSs.js"></script> -->
 
 
-    <script src="../js/cookiealert.js"></script>
+<script src="../js/cookiealert.js"></script>
